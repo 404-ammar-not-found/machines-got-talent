dc18d5a3a5cb377b895102f976fea0dbd9713279
Haris <haris.wahab@student.manchester.ac.uk>
2026-02-26 17:30:48 +0000

    added go websocket steup files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/404-ammar-not-found/machines-got-talent
 
 go 1.25.0
+
+require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	fmt.Println("Client connected!")
+
+	for {
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Printf("Recv: %s\n", message)
+		ws.WriteMessage(mt, message)
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+	fmt.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
