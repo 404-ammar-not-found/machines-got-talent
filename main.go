f2f36bea31c35357199296bbf2f164758eb42227
Haris <haris.wahab@student.manchester.ac.uk>
2026-03-02 05:04:41 +0000

    Added hub system to websocket to handle registering and unregistering unique connections

--- main.go
+++ main.go
@@ -12,27 +12,79 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
-func handleConnections(w http.ResponseWriter, r *http.Request) {
+func handleConnections(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
-	defer ws.Close()
 
-	fmt.Println("Client connected!")
+	hub.register <- ws
+
+	defer func() {
+		hub.unregister <- ws
+	}()
 
 	for {
-		mt, message, err := ws.ReadMessage()
+		_, message, err := ws.ReadMessage()
 		if err != nil {
 			break
 		}
-		fmt.Printf("Recv: %s\n", message)
-		ws.WriteMessage(mt, message)
+		hub.broadcast <- message
+	}
+}
+
+type Hub struct {
+	clients    map[*websocket.Conn]Judge
+	register   chan *websocket.Conn
+	unregister chan *websocket.Conn
+	broadcast  chan []byte
+	totalScore int
+}
+
+type Judge struct {
+	ID      string
+	Name    string
+	InLobby bool
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = Judge{Name: "Anonymous"}
+			fmt.Println("New Judge has entered the lobby!")
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				client.Close()
+				fmt.Println("Judge has left the lobby")
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				client.WriteMessage(websocket.TextMessage, message)
+			}
+
+		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/ws", handleConnections)
+
+	hub := &Hub{
+		clients:    make(map[*websocket.Conn]Judge),
+		register:   make(chan *websocket.Conn),
+		unregister: make(chan *websocket.Conn),
+		broadcast:  make(chan []byte),
+	}
+
+	go hub.run()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		handleConnections(hub, w, r)
+	})
+
 	fmt.Println("Server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
