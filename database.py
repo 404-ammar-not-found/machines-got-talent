06696166f0656658bb57de7fe1137f95fde59143
Tyler <tyler.butters@student.manchester.ac.uk>
2026-03-27 12:00:08 +0000

    Update database.py
    Updated to match main (excl remote db connection)

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
-import mysql.connector, jwt, os
+# app/database.py
+import mysql.connector
+import jwt
+import os
 from dotenv import load_dotenv
+from app.config import settings
 
 """
 Prompts Table
@@ -12,289 +16,221 @@ Attributes:
 id (Integer) (Primary Key) (Unique)
 username (String) (Unique)
 email (String)
-win count (Integer)
+win_count (Integer)
 balance (Integer)
 """
 
 # Class that represents problems encountered while connecting to the database
 class ConnectionError(Exception):
-	def __init__(self, message):
-		self.message = message
-		super().__init__(self.message)
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 # Class that represents problems encountered while accessing the database
 class AccessError(Exception):
-	def __init__(self, message):
-		self.message = message
-		super().__init__(self.message)
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 # Class that represents problems encountered while accessing a token
 class TokenError(Exception):
-	def __init__(self, message):
-		self.message = message
-		super().__init__(self.message)
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 def getUserDetails(token, key):
-	"""
-	Takes in a token and key
-	Outputs the username (String) and email (String) of the account
-	"""
-	try:
-		payload = jwt.decode(token, key, algorithms = ["Argon2"])
-		username = payload["username"]
-		email = payload["email"]
-	except:
-		raise TokenError("There was a problem accessing the user token")
-	else:
-		return username, email
+    """
+    Takes in a token and key
+    Outputs the username (String) and email (String) of the account
+    """
+    try:
+        # NOTE: Changed algorithm from "Argon2" to HS256 to match standard JWT usage
+        payload = jwt.decode(token, key, algorithms = ["HS256"])
+        username = payload.get("username")
+        email = payload.get("email")
+        return username, email
+    except Exception as e:
+        raise TokenError(f"There was a problem accessing the user token: {e}")
 
 def connect():
-	"""
-	Starts a connection with the database
-	Outputs the database and a cursor to communicate with the database
-	The connection is closed at the end of every function
-	"""
-	try:
-		load_dotenv()
-		remote_host = os.getenv("HOST")
-		remote_db = os.getenv("DB")
-		remote_user = os.getenv("USER")
-		remote_pass = os.getenv("DB_PASS")
-		db = mysql.connector.connect(host = remote_host, database = remote_db, user = remote_user, password = remote_pass)
-		cursor = db.cursor()
-	except:
-		raise ConnectionError("There was a problem connecting to the database")
-	else:
-		return db, cursor
+    """
+    Starts a connection with the database
+    Outputs the database connection with the remote database and also outputs a cursor to perform sql on the remote database
+    The connection is closed at the end of every function
+    """
+    try:
+        load_dotenv()
+        remote_host = os.getenv("HOST")
+        remote_db = os.getenv("DB")
+        remote_user = os.getenv("USER")
+        remote_pass = os.getenv("DB_PASS")
+        db = mysql.connector.connect(host = remote_host, database = remote_db, user = remote_user, password = remote_pass)
+        cursor = db.cursor(dictionary = True) # Use dictionary = True for easier access
+        return db, cursor
+    except Exception as e:
+        raise ConnectionError(f"There was a problem connecting to the database: {e}")
 
 def addPrompt(new_prompt):
-	"""
-	Takes in a prompt (String)
-	Adds a new prompt to the prompts table if unique
-	"""
-	if not searchPrompt("prompt", new_prompt):
-		db, cursor = connect()
-		try:
-			sql = "INSERT INTO prompts (prompt) VALUES (%s)"
-			cursor.execute(sql, (new_prompt,))
-			db.commit()
-			cursor.close()
-			db.close()
-		except:
-			cursor.close()
-			db.close()
-			raise AccessError("There was a problem accessing the database")
-	else:
-		raise AccessError("This prompt already exists")
+    """
+    Takes in a prompt (String)
+    Adds a new prompt to the prompts table if unique
+    """
+    if not searchPrompt("prompt", new_prompt):
+        db, cursor = connect()
+        try:
+            sql = "INSERT INTO prompts (prompt) VALUES (%s)"
+            val = (new_prompt,)
+            cursor.execute(sql, val)
+            db.commit()
+            cursor.close()
+            db.close()
+        except:
+            cursor.close()
+            db.close()
+            raise AccessError("There was a problem accessing the database")
+    else:
+        raise AccessError("This prompt already exists")
 
 def getAllPrompts():
-	"""
-	Outputs all prompts in the prompts table
-	"""
-	db, cursor = connect()
-	try:
-		cursor.execute("SELECT * FROM prompts")
-		result = cursor.fetchall()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
-	else:
-		return result
+    """
+    Outputs all prompts in the prompts table
+    """
+    db, cursor = connect()
+    try:
+        cursor.execute("SELECT * FROM prompts")
+        result = cursor.fetchall()
+        cursor.close()
+        db.close()
+        return result
+    except:
+        cursor.close()
+        db.close()
+        raise AccessError("There was a problem accessing the database")
 
 def searchPrompt(attribute, value):
-	"""
-	Takes in an attribute (String) and value
-	Data type of value must be correct to the attribute
-	Outputs the first matching record (Tuple) found or None if there is no matching record
-	"""
-	db, cursor = connect()
-	try:
-		sql = f"SELECT * FROM prompts WHERE {attribute} = (%s)"
-		cursor.execute(sql, (value,))
-		result = cursor.fetchone()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
-	else:
-		return result
+    """
+    Takes in an attribute (String) and value
+    Outputs the first matching record (Dict) found or None
+    """
+    db, cursor = connect()
+    try:
+        sql = f"SELECT * FROM prompts WHERE {attribute} = %s"
+        val = (value,)
+        cursor.execute(sql, val)
+        result = cursor.fetchone()
+        cursor.close()
+        db.close()
+        return result
+    except:
+        cursor.close()
+        db.close()
+        raise AccessError("There was a problem accessing the database")
 
 def removePrompt(attribute, value):
-	"""
-	Takes in an attribute (String) and value
-	Data type of value must be correct to the attribute
-	Removes the matching record from the prompts table
-	"""
-	db, cursor = connect()
-	try:
-		sql = f"DELETE FROM prompts WHERE {attribute} = (%s)"
-		cursor.execute(sql, (value,))
-		db.commit()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
+    db, cursor = connect()
+    try:
+        sql = f"DELETE FROM prompts WHERE {attribute} = %s"
+        val = (value,)
+        cursor.execute(sql, val)
+        db.commit()
+        cursor.close()
+        db.close()
+    except:
+        cursor.close()
+        db.close()
+        raise AccessError("There was a problem accessing the database")
 
 def updatePrompt(prompt_id, new_prompt):
-	"""
-	Takes in the id (Integer) and prompt (String)
-	Replaces the old prompt with the new prompt
-	"""
-	db, cursor = connect()
-	if not searchPrompt("id", prompt_id):
-		raise AccessError("This prompt does not exist")
-	else:
-		try:
-			sql = "UPDATE prompts SET prompt = (%s) WHERE id = (%s)"
-			cursor.execute(sql, (new_prompt, prompt_id))
-			db.commit()
-			cursor.close()
-			db.close()
-		except:
-			cursor.close()
-			db.close()
-			raise AccessError("There was a problem accessing the database")
+    db, cursor = connect()
+    if not searchPrompt("id", prompt_id):
+        raise AccessError("This prompt does not exist")
+    else:
+        try:
+            sql = "UPDATE prompts SET prompt = %s WHERE id = %s"
+            val = (new_prompt, prompt_id)
+            cursor.execute(sql, val)
+            db.commit()
+            cursor.close()
+            db.close()
+        except:
+            cursor.close()
+            db.close()
+            raise AccessError("There was a problem accessing the database")
 
+# (Users table functions follow your provided logic)
 def addUser(token, key):
-	"""
-	Takes in a user token and a key
-	Decodes the token using the key to get the username and email
-	Adds a new user to the users table if the username is unique
-	"""
-	payload_username, payload_email = getUserDetails(token, key)
-	db, cursor = connect()
-	if not searchUserWithoutToken("username", payload_username):
-		try:
-			sql = "INSERT INTO users (username, email, win_count, balance) VALUES (%s, %s, %s, %s)"
-			cursor.execute(sql, (payload_username, payload_email, 0, 0))
-			db.commit()
-			cursor.close()
-			db.close()
-		except:
-			cursor.close()
-			db.close()
-			raise AccessError("There was a problem accessing the database")
-	else:
-		raise AccessError("This user account already exists")
+    payload_username, payload_email = getUserDetails(token, key)
+    db, cursor = connect()
+    if not searchUserWithoutToken("username", payload_username):
+        try:
+            sql = "INSERT INTO users (username, email, win_count, balance) VALUES (%s, %s, %s, %s)"
+            val = (payload_username, payload_email, 0, 0)
+            cursor.execute(sql, val)
+            db.commit()
+            cursor.close()
+            db.close()
+        except:
+            cursor.close()
+            db.close()
+            raise AccessError("There was a problem accessing the database")
+    else:
+        raise AccessError("This user account already exists")
 
 def getAllUsers():
-	"""
-	Outputs all user accounts in the users table
-	"""
-	db, cursor = connect()
-	try:
-		cursor.execute("SELECT * FROM users")
-		result = cursor.fetchall()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
-	else:
-		return result
-
-def searchUserWithToken(token, key):
-	"""
-	Takes in a token and key
-	Outputs the first matching record (Tuple) with the username obtained from the token
-	"""
-	username, email = getUserDetails(token, key)
-	return searchUserWithToken("username", username)
+    db, cursor = connect()
+    try:
+        cursor.execute("SELECT * FROM users")
+        result = cursor.fetchall()
+        cursor.close()
+        db.close()
+        return result
+    except:
+        cursor.close()
+        db.close()
+        raise AccessError("There was a problem accessing the database")
 
 def searchUserWithoutToken(attribute, value):
-	"""
-	Takes in an attribute (String) and a value (String) for that attribute
-	Outputs the first matching record (Tuple) or None if there is no matching record
-	Avoid using email, win count and balance as they are not unique
-	"""
-	db, cursor = connect()
-	try:
-		sql = f"SELECT * FROM users WHERE {attribute} = (%s)"
-		cursor.execute(sql, (value,))
-		result = cursor.fetchone()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
-	else:
-		return result
+    db, cursor = connect()
+    try:
+        sql = f"SELECT * FROM users WHERE {attribute} = %s"
+        val = (value,)
+        cursor.execute(sql, val)
+        result = cursor.fetchone()
+        cursor.close()
+        db.close()
+        return result
+    except:
+        cursor.close()
+        db.close()
+        raise AccessError("There was a problem accessing the database")
 
 def removeUser(attribute, value):
-	"""
-	Takes in an attribute (String) and value (String)
-	Removes the matching record from the users table
-	"""
-	db, cursor = connect()
-	try:
-		sql = f"DELETE FROM users WHERE {attribute} = (%s)"
-		cursor.execute(sql, (value,))
-		db.commit()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
+    db, cursor = connect()
+    try:
+        sql = f"DELETE FROM users WHERE {attribute} = %s"
+        val = (value,)
+        cursor.execute(sql, val)
+        db.commit()
+        cursor.close()
+        db.close()
+    except:
+        cursor.close()
+        db.close()
+        raise AccessError("There was a problem accessing the database")
 
 def updateUser(username, attribute, value):
-	"""
-	Takes in a username (String), attribute (String) and value
-	Data type of value must be correct to the attribute
-	Replaces the old value of the attribute of the user with the new value
-	"""
-	db, cursor = connect()
-	if not searchUser("username", payload_username):
-		raise AccessError("This user account does not exist")
-	else:
-		try:
-			sql = f"UPDATE users SET {attribute} = (%s) WHERE username = (%s)"
-			cursor.execute(sql, (value, username))
-			db.commit()
-			cursor.close()
-			db.close()
-		except:
-			cursor.close()
-			db.close()
-			raise AccessError("There was a problem accessing the database")
-
-def deletePromptsTable():
-	"""
-	Deletes the prompts table
-	"""
-	db, cursor = connect()
-	try:
-		sql = "DROP TABLE IF EXISTS prompts"
-		cursor.execute(sql)
-		db.commit()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
-
-def deleteUsersTable():
-	"""
-	Deletes the users table
-	"""
-	db, cursor = connect()
-	try:
-		sql = "DROP TABLE IF EXISTS users"
-		cursor.execute(sql)
-		db.commit()
-		cursor.close()
-		db.close()
-	except:
-		cursor.close()
-		db.close()
-		raise AccessError("There was a problem accessing the database")
+    db, cursor = connect()
+    if not searchUserWithoutToken("username", username):
+        raise AccessError("This user account does not exist")
+    else:
+        try:
+            sql = f"UPDATE users SET {attribute} = %s WHERE username = %s"
+            val = (value, username)
+            cursor.execute(sql, val)
+            db.commit()
+            cursor.close()
+            db.close()
+        except:
+            cursor.close()
+            db.close()
+            raise AccessError("There was a problem accessing the database")
